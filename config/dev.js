e5bb462860dedbf888cd2b363205892674ce2a51
YOONSU PARK <shinebytheriver@yahoo.com>
2018-11-14 21:09:49 -0800

    finished-auth2

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js - don't commit this!
+
+module.exports = {
+    googleClientID: '1076369128511-kr18kccs8sv9hdv0hkl7if0b1vlaa8kn.apps.googleusercontent.com',
+    googleClientSecret: 'WzZq6wSeEb9IAWA066IpFpNa',
+    mongoURI: 'mongodb://smarts:smarts1@ds161183.mlab.com:61183/emailydev',
+    cookieKey: 'fifheksitkshjteishiehrhahreoldhrteki'  //my arbitrary
+};
