7949802e631072b8568d3e9b93bdcdc931842dbe
YOONSU PARK <shinebytheriver@yahoo.com>
2018-11-15 09:00:02 -0800

    revised_auth_flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //keys.js - figure out wha set of credentials to return
 //NODE_ENV tells whether it is production or development
-if (process.env.NODE_ENV === 'productions') {
+if (process.env.NODE_ENV === 'production') {
   // we are in production -retun the prod set of keys
   module.exports = require('./prod');
 } else {
